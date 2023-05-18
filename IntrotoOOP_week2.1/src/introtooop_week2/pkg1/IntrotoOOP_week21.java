8807ca94f21e4fda8139cca0d5ed5ad4126862b8
BrianaNzivu <bjnzivu@gmail.com>
2023-05-18 10:43:12 +0300

    Update on week 2.1

--- IntrotoOOP_week2.1/src/introtooop_week2/pkg1/IntrotoOOP_week21.java
+++ IntrotoOOP_week2.1/src/introtooop_week2/pkg1/IntrotoOOP_week21.java
@@ -11,30 +11,48 @@ public class IntrotoOOP_week21 {
     
     private int eNumber;
     private String eName;
-
-    public void inputData(){
-        Scanner s = new Scanner(System.in);
+    private int eSalary;
+    private int payrise;
+    
+    IntrotoOOP_week21 (){
+     Scanner s = new Scanner(System.in);
         
         System.out.println("Enter name: ");
         eName = s.next();
         
         System.out.println("Enter number: ");
-        eNumber = s.nextInt();
+        int x = s.nextInt();
+        if (x>999){
+             System.out.println("ERROR:NUMBER SHOULD BE LESS THAN 1000");
+        }
+        else{
+        eNumber = x;}
+        
+        System.out.println("Enter Salary: ");
+        eSalary = s.nextInt();
     }
-    
+
     public void display(){
         System.out.println("Name is: "+ eName + " Number is: " + eNumber);
+    }
+    
+    public void payRise(){
+        payrise = ((110 * eSalary)/100);
+        System.out.println("Salary after payrise is: "+ payrise);
     }
     
     public static void main(String[] args) {
         IntrotoOOP_week21  empOne = new IntrotoOOP_week21();
         IntrotoOOP_week21 empTwo = new IntrotoOOP_week21();
+        IntrotoOOP_week21 empThree = new IntrotoOOP_week21();
         
-        empOne.inputData();
         empOne.display();
+        empOne.payRise();
         
-        empTwo.inputData();
         empTwo.display();
+        empTwo.payRise();
+        
+        empThree.display();
     }
     
 }
---
+++ toberenamed/src/toberenamed/Toberenamed.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package toberenamed;
+
+/**
+ *
+ * @author bnzivu
+ */
+public class Toberenamed {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
